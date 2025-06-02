5357e3a048f88b4cee8bccc07d81e2b2f296cfef
dhruvguptashorthills <dhruv.gupta@shorthills.ai>
2025-06-02 16:15:24 +0530

    Integrating upload resume page to database managment page

--- main.py
+++ main.py
@@ -332,7 +332,7 @@ def validate_and_reprocess_resumes(uploaded_files):
 
             # Check if 'name' is missing
             if not resume_data.get("name") or len(resume_data.get("name").split()[0]) < 2:
-                st.warning(f"⚠️ Missing 'name' in {file_path.name}. Reprocessing...")
+                st.warning(f"{file_path.name}. Reprocessing...")
                 reprocessed_count += 1
 
                 # Find the original file in uploaded files
